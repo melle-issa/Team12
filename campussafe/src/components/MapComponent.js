872515ec4ab485d573c0156ce4b7699a7be41bde
jgarc594 <jgarc594@ucr.edu>
2023-12-12 17:39:23 -0800

    Added firebase

--- campussafe/src/components/MapComponent.js
+++ campussafe/src/components/MapComponent.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './MapComponent.css';
 
 const MapComponent = () => {
   return (
     <div>
       <main>
-        <img id="ucr_map" src="/assets/ucr_map.png" alt="ucr_map_img" />
+        <img id="ucr_map" src="assets/ucr_map.png" alt="ucr_map_img" />
       </main>
     </div>
   );
---
+++ campussafe/src/components/SignUp.js
@@ -0,0 +1,30 @@
+// SignUp.js
+import React, { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = async () => {
+    const auth = getAuth();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      console.log('User registered:', user);
+    } catch (error) {
+      console.error('Error signing up:', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleSignUp}>Sign Up</button>
+    </div>
+  );
+};
+
+export default SignUp;
