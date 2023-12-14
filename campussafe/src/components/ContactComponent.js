dd99edac96afbccf61cfcbc69cd7d3b5b53ec0ed
jgarc594 <jgarc594@ucr.edu>
2023-12-14 11:30:00 -0800

    Added settings and contact page

--- campussafe/src/components/ContactComponent.js
+++ campussafe/src/components/ContactComponent.js
@@ -5,9 +5,65 @@ import './ContactComponent.css';
 const ContactComponent = () => {
   return (
     <div>
+      <header>
+        <div>CampusSafe</div>
+        <a href="#" id="settingsLink">
+          <img id="settingsIcon" src="assets/settings.png" alt="settings" />
+          <div id="settingsDropdown">
+            <a href="settings.html">Settings</a>
+            <a href="profile.html">Profile</a>
+            <a href="index.html">Logout</a>
+          </div>
+        </a>
+      </header>
+
       <main>
-        {/* Add content for the Emergency Contact page */}
+        <br />
+        <br />
+        <h1>
+          IF YOU ARE IN IMMEDIATE DANGER, DIAL{' '}
+          <a href="tel:911" style={{ color: 'black' }}>
+            911
+          </a>
+          .
+        </h1>
+        <h2>UCR Police NON-EMERGENCY Line:</h2>
+        <div className="bubble-container">
+          <div className="bubble">
+            <a href="tel:951-827-5222">951-827-5222</a>
+          </div>
+        </div>
+        <h2>UCR 24/7 Crisis Counselor</h2>
+        <div className="bubble-container">
+          <div className="bubble">
+            <a href="tel:951-827-8255">951-827-8255</a>
+          </div>
+        </div>
+        <h2>24/7 Nurse Line</h2>
+        <div className="bubble-container">
+          <div className="bubble">
+            <a href="tel:877-351-3457">877-351-3457</a>
+          </div>
+        </div>
+        <h2>UCR Basic Needs</h2>
+        <div className="bubble-container">
+          <div className="bubble">
+            <a href="tel:951-827-3663">951-827-3663</a>
+          </div>
+        </div>
       </main>
+
+      <footer>
+        <a href="map.html">
+          <img id="icon1" src="assets/location.png" alt="map" />
+        </a>
+        <a href="contact.html">
+          <img id="icon2" src="assets/calling.png" alt="calling" />
+        </a>
+        <a href="information.html">
+          <img id="icon3" src="assets/info.png" alt="information" />
+        </a>
+      </footer>
     </div>
   );
 };
--- campussafe/src/components/SettingsComponent.js
+++ campussafe/src/components/SettingsComponent.js
@@ -1,10 +1,74 @@
 import React from 'react';
+import './SettingsComponent.css'; // Make sure to include your CSS file
 
 const SettingsComponent = () => {
   return (
     <div>
-      <h1>Settings</h1>
-      {/* Add your settings content here */}
+      <main>
+        <div class="settings-section">
+          <h2>Profile Settings</h2>
+          <label for="profilePicture">Profile Picture</label>
+          <input type="file" id="profilePicture" />
+
+          <label for="name">Name</label>
+          <input type="text" id="name" />
+
+          <label for="email">Email</label>
+          <input type="email" id="email" />
+
+          <label for="password">Password</label>
+          <input type="password" id="password" />
+
+          <button id="saveChangesButton">Save Changes</button>
+        </div>
+
+        <div class="settings-section">
+          <div class="green-bubble">
+            <div class="toggle-content">
+              <div class="alert-types">
+                <div class="alert-type">
+                  <div class="toggle-content">
+                    <h2>Notification Preferences</h2>
+                    <p>Receive Emergency Alerts</p>
+                  </div>
+                  <label class="toggle-switch">
+                    <input type="checkbox" id="emergencyAlerts" />
+                    <span class="slider"></span>
+                  </label>
+                </div>
+                {/* ... (Continue with other alert types) */}
+              </div>
+              <hr class="divider" />
+              <div class="general-alerts">
+                <div class="alert-type">
+                  <label for="updates">Receive General Notifications</label>
+                  <label class="toggle-switch">
+                    <input type="checkbox" id="updates" />
+                    <span class="slider"></span>
+                  </label>
+                </div>
+                {/* ... (Continue with other general alerts) */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="settings-section">
+          <div class="green-bubble">
+            <div class="toggle-content">
+              {/* Add your content for the green bubble here */}
+            </div>
+          </div>
+        </div>
+
+        <div class="settings-section">
+          <div class="green-bubble">
+            <div class="toggle-content">
+              {/* Add your content for the green bubble here */}
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
